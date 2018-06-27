0a8ff2f9e67baeec4bd57953695e7772c0ab7fb0
Glenn Lanzer <lanzertrey@gmail.com>
2018-06-27 01:58:29 -0500

    Add (lessons): Challenges/01-../00-Basic-Javascript #39 and #40

---
+++ Challenges/01-Javascript-Algorithms-and-Data-Structures/00-Basic-Javascript/39-Manipulate-Arrays-With-push/Manipulate-Arrays-With-push.js
@@ -0,0 +1,8 @@
+var myArray = [
+    ["John", 23],
+    ["cat", 2]
+];
+
+myArray.push(["dog", 3]);
+
+// .push() appends a value to the end of an array.
---
+++ Challenges/01-Javascript-Algorithms-and-Data-Structures/00-Basic-Javascript/40-Manipulate-Arrays-With-pop/Manipulate-Arrays-With-pop.js
@@ -0,0 +1,16 @@
+var ourArray = [1,2,3];
+var removedFromOurArray = ourArray.pop();
+// removedFromOurArray = 3
+// ourArray = [1,2]
+
+var myArray = [
+    [
+        "John", 23
+    ],
+    [
+        "cat", 2
+    ]
+];
+var removedFromMyArray = myArray.pop();
+// removedFromMyArray = ["cat", 2]
+// myArray = [["John", 23]]
